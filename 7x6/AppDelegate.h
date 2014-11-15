ec18502d961950c5a8e3c19b0e3c4419a2892942
Nathaniel Mendoza <namendoz@asu.edu>
2014-11-14 20:20:34 -0700

    + initial xcode commit

---
+++ 7x6/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  7x6
+//
+//  Created by Nathaniel Mendoza on 11/14/14.
+//  Copyright (c) 2014 CSE494Project2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
