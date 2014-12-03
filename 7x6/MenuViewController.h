82f2b8a44f2c4d3347d030952a785a1d4125abc7
Nathaniel Mendoza <namendoz@asu.edu>
2014-12-03 16:05:13 -0700

    + initial logging in works

---
+++ 7x6/MenuViewController.h
@@ -0,0 +1,14 @@
+//
+//  MenuViewController.h
+//  7x6
+//
+//  Created by Nathaniel Mendoza on 12/3/14.
+//  Copyright (c) 2014 CSE494Project2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+
+@interface MenuViewController : UIViewController <PFLogInViewControllerDelegate, PFSignUpViewControllerDelegate>
+
+@end
