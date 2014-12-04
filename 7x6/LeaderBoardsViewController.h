b59d3e771f9fa4cba28d9ff6dfffb2d87860fe82
Nathaniel Mendoza <namendoz@asu.edu>
2014-12-03 17:06:01 -0700

    + high scores table works

---
+++ 7x6/LeaderBoardsViewController.h
@@ -0,0 +1,13 @@
+//
+//  LeaderBoardsViewController.h
+//  7x6
+//
+//  Created by Nathaniel Mendoza on 12/3/14.
+//  Copyright (c) 2014 CSE494Project2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LeaderBoardsViewController : UIViewController
+
+@end
